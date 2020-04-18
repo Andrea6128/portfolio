8cb06ff58ee7327f7f535201cab3caaff6b8f82b
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-04-18 10:47:57 +0200

    styled footer and portfolio section

--- project/settings.py
+++ project/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'portfolio',
-        'USER': 'andrea',
-        'PASSWORD': 'hgtr',
+        'USER': '',
+        'PASSWORD': '',
 
         'HOST': '127.0.0.1',
         'PORT': '5432',
